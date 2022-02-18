78d9fab3a379e7ed7fbd0a64aba735e9053645f8
Agatha28 <agasantoso7@gmail.com>
2022-02-18 15:04:37 +0700

    Change Data

--- akunt-api/app/Http/Controllers/TransactionController.php
+++ akunt-api/app/Http/Controllers/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use Symfony\Component\HttpFoundation\Response;
 
 class TransactionController extends Controller
 {
@@ -19,6 +20,8 @@ class TransactionController extends Controller
             'message' => 'List transaction order by time',
             'data' => $transaction
         ];
+        
+        return response()->json($response, Response)
     }
 
     
